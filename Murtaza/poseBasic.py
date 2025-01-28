686e8c78792d742ef6835140b4a965e195a5f2d2
JBelmont72 <130615405+JBelmont72@users.noreply.github.com>
2025-01-27 21:46:46 -0500

    added poseModule

--- Murtaza/poseBasic.py
+++ Murtaza/poseBasic.py
@@ -6,16 +6,22 @@ def __init__(self,static_image_mode=False, model_complexity=1, smooth_landmarks=
                 smooth_segmentation=True, min_detection_confidence=0.5, min_tracking_confidence=0.5):
 make sure to pass those to self.pose
 
-
-
+static_mode_mode  if False, the detection is used once and then just use tracking until the conficence level is too low,  if this hppens then it goes back to capture again
+https://github.com/google-ai-edge/mediapipe/blob/master/docs/solutions/pose.md
 '''
 import cv2
-import mediapipe as mp
+import mediapipe as mp ## uses bgr
 import time
 mpDraw = mp.solutions.drawing_utils
 mpPose = mp.solutions.pose
-pose = mpPose.Pose()
-cap = cv2.VideoCapture('PoseVideos/3.mp4')
+pose = mpPose.Pose(static_image_mode=False,
+    model_complexity=1,
+    enable_segmentation=False,
+    min_detection_confidence=0.7,
+    min_tracking_confidence=0.5,
+    smooth_landmarks=True)
+# cap = cv2.VideoCapture('PoseVideos/3.mp4')
+cap = cv2.VideoCapture('Images/PoseVid_1.mov')
 pTime = 0
 while True:
     success, img = cap.read()
@@ -26,13 +32,16 @@ while True:
         mpDraw.draw_landmarks(img, results.pose_landmarks, mpPose.POSE_CONNECTIONS)
         for id, lm in enumerate(results.pose_landmarks.landmark):
             h, w, c = img.shape
-            print(id, lm)
+            # print(id, lm)
             cx, cy = int(lm.x * w), int(lm.y * h)
-            cv2.circle(img, (cx, cy), 5, (255, 0, 0), cv2.FILLED)
+            # print([id,cx,cy])
+            cv2.circle(img, (cx, cy), 5, (255, 0, 255), cv2.FILLED)
     cTime = time.time()
     fps = 1 / (cTime - pTime)
     pTime = cTime
+    cv2.rectangle(img,(60,15),(140,60),(125,125,0),-1)
     cv2.putText(img, str(int(fps)), (70, 50), cv2.FONT_HERSHEY_PLAIN, 3,
                 (255, 0, 0), 3)
     cv2.imshow("Image", img)
     cv2.waitKey(1)
+    
---
+++ Murtaza/poseModule.py
@@ -0,0 +1,156 @@
+'''
+min_detection_confidence=0.7,
+min_tracking_confidence=0.7
+additional parmeters for self.Pose
+enable_segmentation
+
+If set to true, in addition to the pose landmarks the solution also generates the segmentation mask. Default to false.
+
+smooth_segmentation
+
+If set to true, the solution filters segmentation masks across different input images to reduce jitter. Ignored if enable_segmentation is false or static_image_mode is true. Default to true.
+
+'''
+
+# import cv2
+# import mediapipe as mp ## uses bgr
+# import time
+# class mpPose():
+#     def __init__(self,static_image=False ,min_detection_confidence=0.7,
+# min_tracking_confidence=0.7):
+#         self.mpDraw = mp.solutions.drawing_utils
+#         self.mpPose = mp.solutions.pose
+#         self.pose = self.mpPose.Pose()
+
+#     def poseDetector(self,img):
+#         imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+#         results = self.pose.process(imgRGB)
+#         # print(results.pose_landmarks)
+#         if results.pose_landmarks:
+#             self.mpDraw.draw_landmarks(img, results.pose_landmarks, self.mpPose.POSE_CONNECTIONS)
+#             for id, lm in enumerate(results.pose_landmarks.landmark):
+#                 h, w, c = img.shape
+#                 # print(id, lm)
+#                 cx, cy = int(lm.x * w), int(lm.y * h)
+#                 # print([id,cx,cy])
+#                 cv2.circle(img, (cx, cy), 5, (255, 0, 255), cv2.FILLED)
+#         return img
+
+
+# def main():
+#     cap = cv2.VideoCapture('Images/PoseVid_1.mov')
+#     pTime = 0
+#     myPose=mpPose()
+#     while True:
+#         success, img = cap.read()
+#         img=myPose.poseDetector(img)
+#         # imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+#         # results = pose.process(imgRGB)
+#         # # print(results.pose_landmarks)
+#         # if results.pose_landmarks:
+#         #     mpDraw.draw_landmarks(img, results.pose_landmarks, mpPose.POSE_CONNECTIONS)
+#         #     for id, lm in enumerate(results.pose_landmarks.landmark):
+#         #         h, w, c = img.shape
+#         #         # print(id, lm)
+#         #         cx, cy = int(lm.x * w), int(lm.y * h)
+#         #         # print([id,cx,cy])
+#         #         cv2.circle(img, (cx, cy), 5, (255, 0, 255), cv2.FILLED)
+#         cTime = time.time()
+#         fps = 1 / (cTime - pTime)
+#         pTime = cTime
+#         cv2.rectangle(img,(60,15),(140,60),(125,125,0),-1)
+#         cv2.putText(img, str(int(fps)), (70, 50), cv2.FONT_HERSHEY_PLAIN, 3,
+#                     (255, 0, 0), 3)
+#         cv2.imshow("Image", img)
+#         cv2.waitKey(1)
+
+
+
+
+# if __name__ == '__main__':
+#     main()
+
+# mp_pose = mp.solutions.pose.Pose(min_detection_confidence=0.7, min_tracking_confidence=0.7)
+# mp_drawing = mp.solutions.drawing_utils
+# cap = cv2.VideoCapture('video_path')
+
+## solutions.pose.Pose()
+
+import matplotlib.pyplot as plt
+import matplotlib.image as pimg  
+import cv2
+import mediapipe as mp ## uses bgr
+import time
+class mpPose():
+    def __init__(self,static_image=False ,min_detection_confidence=0.7,smooth_landmarks=True,enable_segmentation=False,smooth_segmentation=True,
+min_tracking_confidence=0.7,
+    model_complexity=2,):
+        self.mpDraw = mp.solutions.drawing_utils
+        self.mpPose = mp.solutions.pose
+        self.pose = self.mpPose.Pose()
+    def poseDetector(self,img,draw=False,flag=False):
+        imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        results = self.pose.process(imgRGB)
+        if results.pose_landmarks:
+            self.mpDraw.draw_landmarks(img, results.pose_landmarks, self.mpPose.POSE_CONNECTIONS)
+            # self.mpDraw.draw_landmarks(img, results.pose_landmarks, self.mpPose.POSE_CONNECTIONS)
+            # print(results.pose_landmarks) 
+            poseLandmarks=[]
+            poseIndex=[]
+            # for id,ldMk in enumerate(results.pose_landmarks.landmark):
+            #     print(f'for index {id} the values are {ldMk.x,ldMk.y}.')
+            # print(results.pose_landmarks.landmark)        
+            for id, lm in enumerate(results.pose_landmarks.landmark):
+                h, w, c = img.shape
+                # print(id, lm)
+                cx, cy = int(lm.x * w), int(lm.y * h)
+                poseIndex.append(id)
+                poseLandmarks.append([cx,cy])
+            # print(poseIndex)
+            # print(poseLandmarks)      
+            if draw:
+                self.mpDraw.draw_landmarks(
+                img, 
+                results.pose_landmarks, 
+                mp.solutions.pose.POSE_CONNECTIONS,
+                self.mpDraw.DrawingSpec(color=(255, 0, 0), thickness=0, circle_radius=6),
+                self.mpDraw.DrawingSpec(color=(255, 0, 0), thickness=6, circle_radius=0) ) ## the first is circle radius and second is line thickness
+            if flag:     
+                # print([id,cx,cy])
+                cv2.circle(img, (cx, cy), 5, (255, 0, 255), cv2.FILLED)
+        return poseIndex, poseLandmarks
+def main():
+    cap = cv2.VideoCapture('Images/PoseVid_1.mov')
+    pTime = 0
+    myPose=mpPose()
+    while True:
+        success, img = cap.read()
+        poseIndex,poseLandmarks=     myPose.poseDetector(img,draw=True,flag=False)
+        # print(poseIndex,poseLandmarks)
+        # imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        # results = pose.process(imgRGB)
+        # # print(results.pose_landmarks)
+        # if results.pose_landmarks:
+        #     mpDraw.draw_landmarks(img, results.pose_landmarks, mpPose.POSE_CONNECTIONS)
+        #     for id, lm in enumerate(results.pose_landmarks.landmark):
+        #         h, w, c = img.shape
+        #         # print(id, lm)
+        #         cx, cy = int(lm.x * w), int(lm.y * h)
+        #         # print([id,cx,cy])
+        #         cv2.circle(img, (cx, cy), 5, (255, 0, 255), cv2.FILLED)       
+        if poseIndex!=None:
+            for i in range(10,20,2):
+                x=poseLandmarks[i]
+                cv2.circle(img,x,25,(255,255,0),-1) 
+        cTime = time.time()
+        fps = 1 / (cTime - pTime)
+        pTime = cTime
+        cv2.rectangle(img,(60,15),(140,60),(125,125,0),-1)
+        cv2.putText(img, str(int(fps)), (70, 50), cv2.FONT_HERSHEY_PLAIN, 3,
+                    (255, 0, 0), 3)
+        cv2.imshow("Image", img)
+        cv2.waitKey(1) 
+if __name__ == '__main__':
+    main()
+    
+    
